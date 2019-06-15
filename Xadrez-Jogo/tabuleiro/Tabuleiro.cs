60671f69bca4ef3620398ea0030e3f3f6acc9869
Magno Salim <salim.magno@gmail.com>
2019-06-15 11:16:51 -0400

    Tratando erros com exceção personalizadas

--- Xadrez-Jogo/tabuleiro/Tabuleiro.cs
+++ Xadrez-Jogo/tabuleiro/Tabuleiro.cs
@@ -16,15 +16,49 @@ namespace tabuleiro
             pecas = new Peca[linhas, colunas];
         }
 
+        /*--- Metodo para adicionar uma linha e uma coluna na Matriz <<Peca>> ---*/
         public Peca peca(int linha, int coluna)
         {
             return pecas[linha, coluna];
         }
-
+        /*--- Metodo para adicionar uma linha e uma coluna na Matriz <<Peca>> ---*/
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+        /*--- Metodo para validar se existe peca na posicao desejada ---*/
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+        /*--- Metodo para colocar uma peca na posicao <<pos>>---*/
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
+        /*--- Metodo para verificar a posicão ---*/
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        /*Metodo para validar a posicao*/
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posicão inválida!");
+            }
+        }
+
     }
 }
